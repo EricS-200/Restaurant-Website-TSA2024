623bf0576ed92b6fd607b30903849a46dc20389d
dwang4002 <161667987+dwang4002@users.noreply.github.com>
2024-11-27 17:04:33 -0500

    loading screen

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,1 +1,60 @@
 
+"use client";
+
+import { useRef, useState, useLayoutEffect, useEffect } from "react";
+import { cn } from "@/utils/utils";
+
+
+export default function LoadItems({items, loading}) {
+  
+    //declaring factual funs
+    const [funFact, setFunFact] = useState("");
+    const funStuff = ["🍇", "🥒", "🍅", "🍔<-veggie"];
+
+    useEffect(() => {
+        const funFactInterval = setInterval(() => {
+          setFunFact(funStuff[Math.floor(Math.random() * funStuff.length)]);
+        }, 2000); //update 2 secs
+    
+        // clean
+        return () => {
+          clearInterval(funFactInterval);
+        };
+      }, []);
+
+    if(loading) {
+            return (
+                <section>
+                    <img src="https://media2.giphy.com/media/d9Hhu2N1KTF0uW76WQ/200w.gif?cid=6c09b952hspy3uk7xedypad6jts1cw08mqw5v8jke50m6nm1&ep=v1_gifs_search&rid=200w.gif&ct=g"/>
+                    <p>Fun Fact:  {funFact}</p>
+                </section>
+            );
+    } 
+        return (items);
+    
+
+  
+}
+
+
+
+
+/* example code
+
+    import LoadItems from "./LoadItems";
+
+    export default function ThingyMaJob() {
+        const [loading, setLoading] = useState(true);
+
+        return(
+            <LoadItems items = {
+                SCREEN HERE
+            } loading = {loading} />
+        )
+        
+        ----------- SOME ASYNC STUFF HERE----------------
+
+        setLoading(false);
+    }
+
+*/
