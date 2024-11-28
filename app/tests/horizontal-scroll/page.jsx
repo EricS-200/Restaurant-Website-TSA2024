c39cc93cff47bb0e78f4ef173a169acf095a39e5
burntnotbaked <lilshadowthesecond@gmail.com>
2024-11-27 22:00:19 -0500

    please help
    playing around with footers trying to learn react DO NOT MERGE!!!!!!!!!

--- app/tests/horizontal-scroll/page.jsx
+++ app/tests/horizontal-scroll/page.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import HorizontalScroll from "@/components/HorizontalScroll";
 import food from "@/public/placeholders/Vegetarian-Food-Bowl.jpg";
-
 export default function Home() {
   return (
     <main className=" pt-64 pb-4">
---
+++ components/ignorethis.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from 'react';
+import Link from "next/link";
+
+/*
+known problem: idk what im doing
+*/
+export default function IgnoreThis() {
+    return(
+        <footer className=" bg-[#104040] w-full text-[ffffff] flex">
+            <div className="flex justify-around justify-items-center">
+                <h2 className="pl-1 text-xl font-bold mt-1">
+                    Filler Text
+                </h2>
+                <p className="flex items-center mb-1 text-center">
+                    <Link
+                        target="_blank"
+                        href="https://www.desmos.com/calculator?=nographpaper"
+                    >
+                        Grug
+                    </Link>
+                </p>
+            </div>
+        </footer>
+    )
+}
