9b28dfceb5dda33841afc96fde95376b9f9546e5
Eric-NoBraincells <eeeric4321@gmail.com>
2024-12-23 19:52:45 -0500

    poppins font added as main font

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import HorizontalScroll from "@/components/HorizontalScroll";
 import food from "@/public/placeholders/Vegetarian-Food-Bowl.jpg";
+import ParallaxBanner from "@/components/SimpleParallaxBanner";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="">
+      <ParallaxBanner />
+
+      <section className="bg-gray-200 w-full flex flex-col items-center">
+        <h2 className="text-6xl font-bold uppercase">Our Story</h2>
+        <p>
+          Learn about the benefits and applications of wind & turbine-based
+          energy in our rapidly changing world.
+        </p>
+      </section>
+    </main>
+  );
 }
