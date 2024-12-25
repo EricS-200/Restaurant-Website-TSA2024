39cbbc39f5846d2e58a68197763c2b28e96c6fc3
burntnotbaked <lilshadowthesecond@gmail.com>
2024-12-24 20:02:41 -0500

    added padding for the footer
    as advertised

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,17 +9,17 @@ known problem: idk what im doing
 export default function IgnoreThis() {
     return(
         <footer className=" bg-slate-900 w-full text-[ffffff] flex-col">
-            <div className="flex justify-around text-slate-300">
+            <div className="flex justify-around text-slate-300 pl-32 py-8">
                 <div className="flex-col w-1/4 content-evenly">
-                    <h2 className="pl-1 text-xl font-bold mt-1">
+                    <h2 className="pl-1 pd-4 text-xl font-bold mt-1">
                         Filler Text
                     </h2>
-                    <h2 className="pl-1 text-xl font-bold mt-1">
+                    <h2 className="pl-1 pd-4 text-xl font-bold mt-1">
                         Filler Text 2
                     </h2>
                 </div>
                 <div className="flex-col w-1/4 content-evenly">
-                    <p className="flex items-center mb-1 text-center">
+                    <p className="flex pd-4 items-center mb-1 text-center">
                         <Link
                             target="_blank"
                             href="https://www.google.com"
@@ -28,7 +28,7 @@ export default function IgnoreThis() {
                         Four
                         </Link>
                     </p>
-                    <p className="flex items-center mb-1 text-center">
+                    <p className="flex pd-4 items-center mb-1 text-center">
                         <Link
                             target="_blank"
                             href=""
@@ -39,15 +39,15 @@ export default function IgnoreThis() {
                     </p>
                 </div>
                 <div className="flex-col w-1/4 content-evenly">
-                    <h2 className="pl-1 text-xl font-bold mt-1">
+                    <h2 className="pl-1 pd-4 text-xl font-bold mt-1">
                         Filler Text 3
                     </h2>
-                    <h2 className="pl-1 text-xl font-bold mt-1">
+                    <h2 className="pl-1 pd-4 text-xl font-bold mt-1">
                         Filler Text 4
                     </h2>
                 </div>
                 <div className="flex-col w-1/4 content-evenly">
-                    <p className='flex items-center mb-1 text-center text-slate-300'>
+                    <p className='flex pd-4 items-center mb-1 text-center text-slate-300'>
                         <Link
                             target="_blank"
                             href="https://www.google.com"
@@ -56,7 +56,7 @@ export default function IgnoreThis() {
                         Six
                         </Link>
                     </p>
-                    <p className='flex items-center mb-1 text-center text-slate-300'>
+                    <p className='flex pd-4 items-center mb-1 text-center text-slate-300'>
                         <Link
                             target="_blank"
                             href="https://www.twitch.tv/filian"
