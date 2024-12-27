03d285c10d3e6738bc7ef1339d5caf1e359cf9f1
Eric-NoBraincells <eeeric4321@gmail.com>
2024-12-27 17:14:07 -0500

    fix + merge statistics section into home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import Button from "@/components/Button";
 import ParallaxBanner from "@/components/SimpleParallaxBanner";
 import HorizontalScroll from "@/components/HorizontalScroll";
+import AnimatedNumber from "@/components/AnimatedNumber";
 
 import homeBanner from "@/public/placeholders/home-banner.jpg"; //placeholder
 
+import logoImage from "/public/logo.png";
 import goldenLeaves from "@/public/golden-leaves1.png";
 import goldenVegetable1 from "@/public/golden-vegetable1.png";
 import goldenVegetable2 from "@/public/golden-vegetable2.png";
@@ -98,6 +100,62 @@ export default function Home() {
         <Image alt="" placeholder="blur" src={menuItem1} />
         <Image alt="" placeholder="blur" src={menuItem1} />
       </HorizontalScroll>
+
+      <section
+        className={"bg-white w-full flex flex-col items-center justify-center"}
+      >
+        <div className={"flex flex-row w-full justify-between items-center "}>
+          <div
+            className={"pl-[12%] h-fit w-1/3 flex items-center justify-center"}
+          >
+            <Image
+              src={logoImage}
+              alt={"logo"}
+              width={300}
+              className={"h-max"}
+            />
+          </div>
+
+          <div
+            className={
+              "px-[12%] flex flex-col w-2/3 text-4xl justify-center pt-[5%]"
+            }
+          >
+            <AnimatedNumber
+              targetNumber={4_326_994_192}
+              targetString={"4,000,000,000"}
+              startingFraction={0.5}
+              duration={1000}
+              extraStyle={"text-7xl"}
+            />
+            <p className={"pb-[10%]"}>
+              What we&#39;re doing to keep abusing children
+            </p>
+
+            <AnimatedNumber
+              targetNumber={4_291_496_623}
+              targetString={"4,000,000,000"}
+              startingFraction={0.5}
+              duration={1000}
+              extraStyle={"text-7xl"}
+            />
+            <p className={"pb-[10%]"}>
+              What we&#39;re doing to keep abusing children
+            </p>
+
+            <AnimatedNumber
+              targetNumber={4_000_000_000}
+              targetString={"4,000,000,000"}
+              startingFraction={0.5}
+              duration={1000}
+              extraStyle={"text-7xl"}
+            />
+            <p className={"pb-[10%]"}>
+              What we&#39;re doing to keep abusing children
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
