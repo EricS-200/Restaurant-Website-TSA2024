30a65726c198e1dfc2a1464a374475fa600105f8
burntnotbaked <lilshadowthesecond@gmail.com>
2024-12-31 14:46:09 -0500

    idfk
    version control happened ig?? bunch of pages from other branches + main just materialized so i might as well roll with it lol

---
+++ app/community/page.jsx
@@ -0,0 +1,8 @@
+export default function community()
+{
+    return  (
+        <div>
+
+        </div>
+    );
+}
---
+++ app/order/page.jsx
@@ -0,0 +1,8 @@
+export default function order()
+{
+    return  (
+        <div>
+
+        </div>
+    );
+}
