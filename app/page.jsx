c0e02b62860b0bb44c3ac728dc2542814527a8dc
burntnotbaked <lilshadowthesecond@gmail.com>
2025-01-11 14:40:30 -0500

    started our story SECTION, not PAGE
    as advertised

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,28 @@
 import Image from "next/image";
 import HorizontalScroll from "@/components/HorizontalScroll";
 import food from "@/public/placeholders/Vegetarian-Food-Bowl.jpg";
-
+import ourstorystripped from "@/public/images/our story stripped.png"
 export default function Home() {
-  return <main className=""></main>;
+  return (
+      <main className="">
+        {
+          // our story section, method 1
+        }
+          <div className="">
+              <h1 className="absolute order-last text-9xl pl-10 pt-10">
+                  Our Story
+                  {
+                      // change this to the right font and shit later
+                  }
+              </h1>
+              <Image
+                  src={ourstorystripped}
+                  alt="its just a png of the section design since everything is static.
+            im just lazy"
+                  className="">
+              </Image>
+
+          </div>
+      </main>
+  );
 }
