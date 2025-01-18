e66b255b311e4fbf9c52759aa59c2456b4e7f965
burntnotbaked <lilshadowthesecond@gmail.com>
2025-01-18 15:20:16 -0500

    copied over footer from my branch
    yea

---
+++ components/StyledFooter.jsx
@@ -0,0 +1,228 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import icon from "@/public/images/icons/craftroots-logo-old-1e5541.png";
+import twitter from "@/public/images/icons/twitter-square-color-icon.png";
+import facebook from "@/public/images/icons/facebook-round-color-icon.png";
+import instagram from "@/public/images/icons/ig-instagram-icon.png";
+import youtube from "@/public/images/icons/youtube-color-icon.png";
+import Image from "next/image";
+
+export default function StyledFooter() {
+    return (
+
+        <footer className="flex flex-col bg-[#d0b577] font-seasons">
+            <div className="w-full flex flex-row justify-start">
+                {
+                    // name and logo div, katherine wants this to scroll left & repeat
+                }
+                <Link
+                    target=""
+                    href="/"
+                    locale=""
+                    className="hover:underline flex flex-row transition-all">
+                    <Image
+                        src={icon}
+                        alt="CraftRoots Logo"
+                        width={100}
+                        height={100}
+                    ></Image>
+                    <h1 className="text-8xl font-bold text-[#03402a] hover:text-[#277156] delay-50 transition-all">
+                        CraftRoots
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex flex-row justify-evenly h-56 text-[#03402a] font-dm">
+                {
+                    // columns div
+                }
+                <div className="flex flex-col place-content-between">
+                    {
+                        // column 1 (our story, community)
+
+                    }
+                    <div className="flex flex-col justify-center text-center">
+                        {
+                            // Our Story
+                        }
+                        <h1 className="text-4xl font-bold">
+                            About Us
+                        </h1>
+                        <p>
+                            <Link
+                                target=""
+                                href="/ourStory"
+                                locale=""
+                                className="hover:underline hover:text-slate-500 transition-all delay-100"
+                            >
+                                Our Story
+                            </Link>
+                        </p>
+                        <p>
+                            <Link
+                                target=""
+                                href="/references"
+                                className="hover:underline hover:text-slate-500 transition-all delay-100"
+                                locale="">
+                                References
+                            </Link>
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-center text-center">
+                        {
+                            // Community
+                        }
+                        <h1 className="text-4xl font-bold">
+                            Community
+                        </h1>
+                        <div className="flex flex-row justify-evenly pb-2">
+                            {
+                                // socials icons
+                            }
+                            <Link
+                                href="https://twitter.com/officialahs_tsa"
+                                target="_blank"
+                                className="hover:scale-110 transition-all"
+                            >
+                                <Image
+                                    src={twitter}
+                                    alt="Twitter Square Icon"
+                                    width="36"
+                                    height="36">
+                                </Image>
+                            </Link>
+                            <Link
+                                href="https://www.youtube.com/@National_TSA"
+                                target="_blank"
+                                className="hover:scale-110 transition-all"
+                            >
+                                <Image
+                                    src={youtube}
+                                    alt="Youtube Icon"
+                                    width="36"
+                                    height="36">
+                                </Image>
+                            </Link>
+                            <Link
+                                href="https://www.instagram.com/ky_tsa/"
+                                target="_blank"
+                                className="hover:scale-110 transition-all"
+                            >
+                                <Image
+                                    src={instagram}
+                                    alt="Instagram Square Icon"
+                                    width="36"
+                                    height="36">
+                                </Image>
+                            </Link>
+                            <Link
+                                href="https://www.facebook.com/thekytsa/"
+                                target="_blank"
+                                className="hover:scale-110 transition-all"
+                            >
+                                <Image
+                                    src={facebook}
+                                    alt="Facebook Round Icon"
+                                    width="36"
+                                    height="36">
+                                </Image>
+                            </Link>
+
+                        </div>
+                        <Link
+                            href="mailto:general@tsaweb.org"
+                            target="_blank"
+                        >
+                            <button
+                                className="px-5 text-3xl bg-lime-800 hover:bg-lime-600 text-green-100 hover:text-white transition-all delay-100 leading-10 rounded-full justify-self-center">
+                                Contact Us
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex flex-col place-content-between">
+                    {
+                        // column 2 (about our food, menu)
+                    }
+                    <div className="flex flex-col justify-center text-center">
+                        {
+                            // About our Food
+                        }
+                        <h1 className="text-4xl font-bold">
+                            About our Food
+                        </h1>
+                        <p>
+                            <Link
+                                target=""
+                                href="/sustainability"
+                                locale=""
+                                className="hover:underline hover:text-slate-500 transition-all delay-100"
+                            >
+                                Sustainability
+                            </Link>
+                        </p>
+                        <p>
+                            <Link
+                                target=""
+                                href="/farmToTable"
+                                locale=""
+                                className="hover:underline hover:text-slate-500 transition-all delay-100"
+                            >
+                                Farm-to-Table
+                            </Link>
+                        </p>
+                        <p>
+                            <Link
+                                target=""
+                                href="/preparation"
+                                locale=""
+                                className="hover:underline hover:text-slate-500 transition-all delay-100"
+                            >
+                                Preparation
+                            </Link>
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-items-center text-center">
+                        {
+                            // Menu
+                        }
+                        <Link
+                            target=""
+                            href="/menu"
+                            locale=""
+                            className="hover:underline hover:text-slate-600 transition-all delay-100"
+                        >
+                            <h1 className="text-4xl font-bold">
+                                Menu
+                            </h1>
+                        </Link>
+                        <Link
+                            target=""
+                            href="/order"
+                            locale=""
+                        >
+                            <button
+                                className="w-44 text-3xl bg-orange-500 hover:bg-orange-400 text-amber-100 hover:text-white transition-all delay-100 leading-10 rounded-full justify-self-center">
+                                Order Now
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="w-3/5 justify-evenly flex flex-row place-self-center py-5">
+                {
+                    // legal & socials div
+                }
+                <p>
+                    Privacy Policy
+                </p>
+                <p>
+                    Terms & Conditions
+                </p>
+                <p>
+                    USDA Permits
+                </p>
+            </div>
+        </footer>
+    )
+}
