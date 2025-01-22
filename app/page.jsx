879a8ffdc467a22763269d8ba095d35ab29d8347
witherhoard99 <151796337+witherhoard99@users.noreply.github.com>
2025-01-22 12:19:56 -0500

    main is very broken rn
    and i am working on fixing it

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="">
       <HomePageBanner />
 
-      {/* Our Story */}
+      {/* Our Story. Does not work with parallax rn*/}
       {ourStorySection()}
 
       <MenuPreview />
