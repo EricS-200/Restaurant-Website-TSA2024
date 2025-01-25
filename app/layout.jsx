e9ca910220b24b79417dcf046281967430f62406
witherhoard99 <151796337+witherhoard99@users.noreply.github.com>
2025-01-25 17:54:59 -0500

    fixes + responsive + changes idk man

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import StyledFooter from "@/components/StyledFooter";
 
 export const metadata = {
   title: "",
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
       <body className={"h-full font-sans"}>
         <Navbar />
         {children}
-        <StyledFooter/>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import Button from "@/components/Button";
-import ParallaxBanner from "@/components/SimpleParallaxBanner";
 import AnimatedNumber from "@/components/AnimatedNumber";
-
 import MenuPreview from "@/components/MenuPreview";
-
-import homeBanner from "@/public/placeholders/home-banner.jpg"; //placeholder
-
 import logoImage from "/public/logo.png";
 
 import ourStoryImage0 from "/public/homepage/ourStoryImage0.png";
 import ourStoryImage1 from "/public/homepage/ourStoryImage1.png";
 import katherinesDumbLeafThatSheReallyWantedAndImReallyTiltedRnINeedToHopOffWebDevScrewWebDevCppIsBetter from "/public/homepage/katherinesDumbLeafThatSheReallyWantedAndImReallyTiltedRnINeedToHopOffWebDevScrewWebDevCppIsBetter.png";
-import tajmahal from "/public/homepage/tajmahal.png";
 
 export default function Home() {
   return (
@@ -114,7 +107,7 @@ function ourStorySection() {
                   Our Story
                 </button>
               </Link>
-              <Link href={"/reference"} className={"absolute bottom-0 left-[max(10vw,35%)] 2xl:left-[min(10vw,20%)] triangle-clip-path"}>
+              <Link href={"/reference"} className={"absolute bottom-0 left-[clamp(10px,max(10vw,35%),250px)] katherine-triangle-clip-path"}>
                 <button className="bg-[#9da57f] hover:bg-[#b0bf75] py-2 px-8 pr-20 text-black text-md md:text-lg">
                   References
                 </button>
@@ -138,111 +131,55 @@ function ourStorySection() {
           />
         </div>
 
-        <div className={"bg-white w-[96%] md:w-[90%] h-max mx-[2%] md:ml-[5%] mt-[10%]"}>
-
-          {/*Desktop Version*/}
-          <div className={"hidden lg:block pb-[15vh] relative"}>
-            <div className={"p-[2%] px-[4%] h-max flex flex-row"}>
-
-              <div className={"w-1/2 relative"}>
-                <h1 className={"text-7xl 2xl:text-8xl text-green-900 font-extrabold"}>
-                  Our Story
-                </h1>
-                <h1 className={"absolute w-[70vw] top-[-25%] xl:top-[-30%] left-[-25%] xl:left-[-20%] font-mistrully opacity-15 text-[10rem] lg:text-[12rem] 2xl:text-[14rem] pointer-events-none select-none text-clip text-green-900"}>
-                  Our Story
-                </h1>
-                <p className={"pt-[5%] pb-[15%] w-[90%] leading-8"}>
-                  Founded in 1999, we at Craftroots have been trying our absolute
-                  hardest to provide the best service possible. From our founders&apos;
-                  humble beginnings of working in South Indian textiles and Chinese
-                  manufacturing industries to their immigration to the United States, we
-                  now look to serve nothing but the best, using nothing but the freshest
-                  ingredients and most talented chefs.
-                </p>
-
-                <Link href={"/our-story"} className={"absolute bottom-0 left-0"}>
-                  <button className="bg-[#d0b577] hover:bg-[#d4b161] py-2 px-8 text-black text-md md:text-lg">
-                    Our Story
-                  </button>
-                </Link>
-                <Link href={"/reference"}
-                      className={"absolute bottom-0 left-[max(10vw,35%)] xl:left-[min(10vw,35%)] triangle-clip-path"}>
-                  <button className="bg-[#9da57f] hover:bg-[#b0bf75] py-2 px-8 pr-20 text-black text-md md:text-lg">
-                    References
-                  </button>
-                </Link>
-              </div>
-
-              <div className={"w-1/2 justify-center items-center relative"}>
-                <div className={"relative w-full h-full flex flex-row justify-center items-center"}>
-                  <div className={"flex h-[min(50vh,100%)]"}>
-                    <Image src={ourStoryImage0} alt={"Image of table with food"}
-                           className={"object-cover object-center katherine-weird-border-rounding-web-dev-sucks-im-tilted-as-you-can-tell-screw-this-lmao-verbose-but-not-as-verbose-as-java"}/>
-                  </div>
-                  <Image src={ourStoryImage1} alt={"Plate with food"}
-                         className={"w-[50%] top-[min(45vh,90%)] xl:top-[min(35vh,70%)] left-[30vh] lg:left-[34vh] xl:left-[37vh] h-auto absolute z-10"}/>
+        {/*Mobile Version*/}
+        <div className={"lg:hidden p-[2%] mt-[10vh] h-max flex flex-col"}>
+          <div className={"p-[2%] px-[1%] h-max flex flex-col"}>
+            <div className={"w-full relative"}>
+              <div className={"relative w-full h-full flex flex-row justify-center items-center"}>
+                <div className={"flex h-[min(50vh,100%)]"}>
+                  <Image src={ourStoryImage0} alt={"Image of table with food"}
+                         className={"object-cover object-center katherine-weird-border-rounding-web-dev-sucks-im-tilted-as-you-can-tell-screw-this-lmao-verbose-but-not-as-verbose-as-java"}/>
                 </div>
+                <Image src={ourStoryImage1} alt={"Plate with food"}
+                       className={"w-[50%] top-[60%] left-[50%] h-auto absolute z-10"}/>
               </div>
-
             </div>
 
-            <Image
-                src={katherinesDumbLeafThatSheReallyWantedAndImReallyTiltedRnINeedToHopOffWebDevScrewWebDevCppIsBetter}
-                alt={"leafImage"}
-                className={"absolute bottom-[-25%] left-[-7vw] w-[20%] h-auto rotate-[35deg] pointer-events-none"}
-            />
-          </div>
+            <div className={"w-full relative"}>
+              <h1 className={"text-7xl text-green-900 font-black pt-[7%]"}>
+                Our
+                <br/> {/*Crimes being commited here. oh well. */}
+                Story
+              </h1>
+              <h1 className={"absolute h-[25%] w-[100vw] top-[5%] left-[-5%] pt-[2%] leading-snug font-mistrully opacity-20 text-[5.5rem] pointer-events-none text-clip text-green-900 overflow-x-hidden"}>
+                Our Story
+              </h1>
+              <p className={"pt-[5%] pb-[45%] leading-8"}>
+                Founded in 1999, we at Craftroots have been trying our absolute
+                hardest to provide the best service possible. From our founders&apos;
+                humble beginnings of working in South Indian textiles and Chinese
+                manufacturing industries to their immigration to the United States, we
+                now look to serve nothing but the best, using nothing but the freshest
+                ingredients and most talented chefs.
 
-          {/*Mobile Version*/}
-          <div className={"lg:hidden p-[2%] mt-[10vh] h-max flex flex-col"}>
-            <div className={"p-[2%] px-[1%] h-max flex flex-col"}>
-              <div className={"w-full relative"}>
-                <div className={"relative w-full h-full flex flex-row justify-center items-center"}>
-                  <div className={"flex h-[min(50vh,100%)]"}>
-                    <Image src={ourStoryImage0} alt={"Image of table with food"}
-                           className={"object-cover object-center katherine-weird-border-rounding-web-dev-sucks-im-tilted-as-you-can-tell-screw-this-lmao-verbose-but-not-as-verbose-as-java"}/>
-                  </div>
-                  <Image src={ourStoryImage1} alt={"Plate with food"}
-                         className={"w-[50%] top-[60%] left-[50%] h-auto absolute z-10"}/>
-                </div>
-              </div>
+                {/*Image being here is the most consistent way to get it to line up*/}
+                <Image
+                    src={katherinesDumbLeafThatSheReallyWantedAndImReallyTiltedRnINeedToHopOffWebDevScrewWebDevCppIsBetter}
+                    alt={"Image of leafs"}
+                    className={"absolute bottom-[-13%] left-[-12vw] w-[50%] h-auto rotate-[35deg]"}
+                />
+              </p>
 
-              <div className={"w-full relative"}>
-                <h1 className={"text-7xl text-green-900 font-black pt-[7%]"}>
-                  Our
-                  <br/> {/*Crimes being commited here. oh well. */}
-                  Story
-                </h1>
-                <h1 className={"absolute h-[25%] w-[100vw] top-[5%] left-[-5%] pt-[2%] leading-snug font-mistrully opacity-20 text-[5.5rem] pointer-events-none text-clip text-green-900 overflow-x-hidden"}>
+              <Link href={"/our-story"} className={"absolute bottom-[12%] left-0"}>
+                <button className="bg-[#d0b577] hover:bg-[#d4b161] py-1 px-4 pr-6 text-black text-md">
                   Our Story
-                </h1>
-                <p className={"pt-[5%] pb-[45%] leading-8"}>
-                  Founded in 1999, we at Craftroots have been trying our absolute
-                  hardest to provide the best service possible. From our founders&apos;
-                  humble beginnings of working in South Indian textiles and Chinese
-                  manufacturing industries to their immigration to the United States, we
-                  now look to serve nothing but the best, using nothing but the freshest
-                  ingredients and most talented chefs.
-
-                  {/*Image being here is the most consistent way to get it to line up*/}
-                  <Image
-                      src={katherinesDumbLeafThatSheReallyWantedAndImReallyTiltedRnINeedToHopOffWebDevScrewWebDevCppIsBetter}
-                      alt={"Image of leafs"}
-                      className={"absolute bottom-[-13%] left-[-12vw] w-[50%] h-auto rotate-[35deg]"}
-                  />
-                </p>
-
-                <Link href={"/our-story"} className={"absolute bottom-[14%] left-0"}>
-                  <button className="bg-[#d0b577] hover:bg-[#d4b161] py-1 px-4 pr-6 text-black text-md">
-                    Our Story
-                  </button>
-                </Link>
-                <Link href={"/reference"} className={"absolute bottom-[14%] left-[40%] triangle-clip-path"}>
-                  <button className="bg-[#9da57f] hover:bg-[#b0bf75] py-1 px-6 pr-14 text-black text-md">
-                    References
-                  </button>
-                </Link>
-              </div>
+                </button>
+              </Link>
+              <Link href={"/reference"} className={"absolute bottom-[12%] left-[clamp(130px,40%,200px)] katherine-triangle-clip-path"}>
+                <button className="bg-[#9da57f] hover:bg-[#b0bf75] py-1 px-6 pr-14 text-black text-md">
+                  References
+                </button>
+              </Link>
             </div>
           </div>
         </div>
