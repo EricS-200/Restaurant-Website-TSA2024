c6f57d1821afbd22913190ace08248c34544cbce
burntnotbaked <lilshadowthesecond@gmail.com>
2025-03-05 21:16:20 -0500

    itty bitty start on community page
    life keeps getting staler and staler

--- app/community/page.jsx
+++ app/community/page.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+import communitybanner from '@/public/images/bannerimages/communitybanner.png';
+import Image from 'next/image';
+
 export default function community()
 {
     return  (
         <div>
-
+            {
+            /*{
+                // Header
+            }
+            <div>
+                <div className="flex p-10 text-2xl font-seasons justify-center">
+                <h1 className='text-8xl font-seasons pt-15'> Community </h1>
+                </div>
+            </div> */}
+            <div className="flex relative items-evenly justify-center">
+                <Image src={communitybanner} alt="Community Banner">
+                </Image>
+            </div>
         </div>
     );
 }
