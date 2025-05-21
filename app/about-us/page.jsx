f5bfc29f8b21ea25bdb7fe25eb83b4a86a8b7696
Eric-NoBraincells <eeeric4321@gmail.com>
2025-05-20 21:59:47 -0400

    about us page, no decorative elements yet but main items are in

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,257 +1,169 @@
-import banner from "@/public/about/banner.png";
-import divider from "@/public/preparation/3.png";
-import Image from "next/image";
-import logo from "@/public/logo.png";
-import kitchen from "@/public/about/kitchen.jpg";
-import earth from "@/public/about/green-earth.jpg";
 import ParallaxBanner from "@/components/SimpleParallaxBanner";
 import Link from "next/link";
-export default function aboutUs() {
+import Image from "next/image";
+
+import image1 from "@/public/about/1.png";
+import image2 from "@/public/about/2.png";
+import image3 from "@/public/about/3.png";
+import image4 from "@/public/about/4.png";
+import image5 from "@/public/about/5.png";
+import image6 from "@/public/about/6.png";
+import image7 from "@/public/about/7.png";
+import image8 from "@/public/about/8.png";
+import image9 from "@/public/about/9.png";
+import image10 from "@/public/about/10.png";
+import image11 from "@/public/about/11.png";
+import image12 from "@/public/about/12.png";
+import image13 from "@/public/about/13.png";
+import image14 from "@/public/about/14.png";
+import image15 from "@/public/about/15.png";
+import image16 from "@/public/about/16.png";
+
+export default function about() {
   return (
-    <main className=" mb-[50px]">
-      <ParallaxBanner src={banner} className="h-screen" speed={0.5}>
-        <div className="absolute inset-0 bg-black/30"></div>
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center w-[95%]">
-          <h1 className="font-kenao text-8xl mb-4">About Us</h1>
-          <h1 className="font-kenao text-2xl">
-            Who we are, what we do, and why we do it.
-          </h1>
+    <main className="relative text-xl leading-loose flex flex-col items-center overflow-x-clip">
+      <section className="relative w-full h-[100vh]">
+        <Image
+          src={image1}
+          alt=""
+          className="object-top object-cover h-full w-full"
+          fill
+        />
+        <div className="w-full h-full absolute inset-0 bg-black/15 "></div>
+        <h1 className="w-full text-center absolute left-1/2 top-1/2 text-white text-9xl lg:text-[10rem] font-dm -translate-x-1/2 -translate-y-1/2 z-50">
+          About Us
+        </h1>
+      </section>
+      <section className="my-8  relative">
+        <div className="text-center relative bg-white py-6">
+          <h3 className="text-nowrap hidden md:block text-[#de7008] font-mistrully opacity-30 text-[10rem] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[40%] select-none pointer-events-none">
+            Our Story
+          </h3>
+          <h2 className="text-[#d17217] text-8xl font-dm">Our Story</h2>
         </div>
-      </ParallaxBanner>
-      <section className="flex items-center justify-center w-full relative">
-        <div className="w-[95%] lg:w-3/4 flex  items-center md:flex-row flex-col ">
-          <div className="md:w-1/2 flex flex-col justify-center">
-            <h2 className="p-4 font-dm text-4xl md:text-6xl text-[#03402a]">
-              Who We Are
-            </h2>
-            <p>
-              Craftroots&apos; team is primarily composed of those with Asian
-              heritage, including members from India, China, Iraq, the
-              Phillippines, and more. We are highly committed to sharing our
-              cultural heritage through cuisine, along with adding our own
-              vegetarian and sustainable twist to it.
+        <div className="flex justify-center items-center mt-6">
+          <div className="flex flex-col lg:flex-row lg:space-x-8 px-8 xl:w-2/3 items-center">
+            <Image src={image2} alt="" className="" />
+            <p className="w-full sm:w-[80%] text-center lg:text-left text-xl leading-loose">
+              Craftroots provides a connection between people, not machines,
+              through every step of the way. From vegetables hand-picked and
+              transported by local farmers to the same vegetables being stocked
+              & tracked by our pantry chef, to the same vegetables being
+              prepared expertly by our chefs,
             </p>
-            <p>
-              We also partner with a multitude of local farms and businesses to
-              best serve the community as a whole in more ways than one.
-            </p>
-          </div>
-          <div className="w-[95%] md:w-1/2  md:w-1/2 items-center justify-center flex ">
-            <Image src={logo} alt="" className="" />
           </div>
         </div>
       </section>
-      <section className="flex items-center justify-center w-full relative">
-        <div className="w-[95%] lg:w-3/4 flex  items-center md:flex-row flex-col ">
-          <div className="w-[95%] md:w-1/2 flex flex-col justify-center  md:order-2 order-1">
-            <h2 className="p-4 font-dm text-4xl md:text-6xl text-[#03402a]">
-              Who We Are
-            </h2>
-            <p>
-              Craftroots&apos; team is primarily composed of those with Asian
-              heritage, including members from India, China, Iraq, the
-              Phillippines, and more. We are highly committed to sharing our
-              cultural heritage through cuisine, along with adding our own
-              vegetarian and sustainable twist to it.
-            </p>
-            <p>
-              We also partner with a multitude of local farms and businesses to
-              best serve the community as a whole in more ways than one.
-            </p>
-          </div>
-          <div className="w-[95%] md:w-1/2  items-center justify-center flex md:order-1 order-2 mr-8">
-            <Image src={kitchen} alt="" className="" />
+      <section className="bg-white mx-4 lg:mx-14 my-4 p-4 lg:p-16">
+        <div className="relative">
+          <h3 className="inset-0 text-center lg:text-left -translate-y-full -translate-x-[5%] text-nowrap hidden md:block text-[#fddc5c] font-mistrully opacity-50 text-[9rem] absolute select-none pointer-events-none">
+            Our Mission
+          </h3>
+          <h2 className="text-[#cfb53b] text-7xl font-dm relative text-center lg:text-left">
+            Our Mission
+          </h2>
+        </div>
+        <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-4 ">
+          <p className="lg:w-1/2 text-xl mt-4 leading-loose text-center lg:text-left">
+            Craftroots provides a connection between people, not machines,
+            through every step of the way. From vegetables hand-picked and
+            transported by local farmers to the same vegetables being stocked &
+            tracked by our pantry chef, to the same vegetables being prepared
+            expertly by our chefs,
+          </p>
+          <div className="lg:w-1/2 h-full flex items-center justify-center">
+            <Image src={image15} alt="" className="lg:max-w-[600px]" />
+            <Image
+              src={image16}
+              alt=""
+              className="absolute right-0 w-[500px] -translate-y-[30%] hidden lg:block"
+            />
           </div>
         </div>
       </section>
-      <section className="flex items-center justify-center w-full relative">
-        <div className="w-[95%] lg:w-3/4 flex  items-center md:flex-row flex-col">
-          <div className="w-[95%] md:w-1/2  flex flex-col justify-center">
-            <h2 className="p-4 font-dm text-4xl md:text-6xl text-[#03402a]">
-              Who We Are
-            </h2>
-            <p>
-              Craftroots&apos; team is primarily composed of those with Asian
-              heritage, including members from India, China, Iraq, the
-              Phillippines, and more. We are highly committed to sharing our
-              cultural heritage through cuisine, along with adding our own
-              vegetarian and sustainable twist to it.
-            </p>
-            <p>
-              We also partner with a multitude of local farms and businesses to
-              best serve the community as a whole in more ways than one.
+      <section className="lg:p-8 2xl:w-[90%] lg:mx-4 space-y-8">
+        <div className="relative my-4 lg:w-2/3 ml-auto">
+          <h3 className="text-center inset-0 -translate-y-full text-nowrap hidden md:block text-[#2c8748] font-mistrully opacity-30 text-[9rem] absolute select-none pointer-events-none">
+            Our Goals
+          </h3>
+          <h2 className="text-[#1e5541] text-7xl font-dm relative text-center">
+            Our Goals
+          </h2>
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row relative">
+          <div className="basis-1/3 flex items-center justify-center order-2 lg:order-1 my-4 lg:my-0">
+            <Image src={image6} className="" alt="" />
+          </div>
+          <div className="basis-2/3 space-y-5 flex flex-col justify-center items-center order-1 lg:order-2">
+            <h4 className="text-center font-kenao text-7xl text-[#2c8748]">
+              Goal I
+            </h4>
+            <p className="text-center max-w-[90%]">
+              Craftroots provides a connection between people, not machines,
+              through every step of the way. From vegetables hand-picked and
+              transported by local farmers to the same vegetables being stocked
+              & tracked by our pantry chef, to the same vegetables being
+              prepared expertly by our chefs,
             </p>
           </div>
-          <div className="w-[95%] md:w-1/2 items-center justify-center flex ml-8">
-            <Image src={earth} alt="" className="" />
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row relative">
+          <div className="basis-1/3 flex items-center justify-center order-2 lg:order-1 my-4 lg:my-0">
+            <Image src={image7} className="" alt="" />
+          </div>
+          <div className="basis-2/3 space-y-5 flex flex-col justify-center items-center order-1 lg:order-2">
+            <h4 className="text-center font-kenao text-7xl text-[#2c8748]">
+              Goal II
+            </h4>
+            <p className="text-center max-w-[90%]">
+              Craftroots provides a connection between people, not machines,
+              through every step of the way. From vegetables hand-picked and
+              transported by local farmers to the same vegetables being stocked
+              & tracked by our pantry chef, to the same vegetables being
+              prepared expertly by our chefs,
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row relative">
+          <div className="basis-1/3 flex items-center justify-center order-2 lg:order-1 my-4 lg:my-0">
+            <Image src={image8} className="" alt="" />
+          </div>
+          <div className="basis-2/3 space-y-5 flex flex-col justify-center items-center order-1 lg:order-2">
+            <h4 className="text-center font-kenao text-7xl text-[#2c8748]">
+              Goal III
+            </h4>
+            <p className="text-center max-w-[90%]">
+              Craftroots provides a connection between people, not machines,
+              through every step of the way. From vegetables hand-picked and
+              transported by local farmers to the same vegetables being stocked
+              & tracked by our pantry chef, to the same vegetables being
+              prepared expertly by our chefs,
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col lg:flex-row relative">
+          <div className="basis-1/3 flex items-center justify-center order-2 lg:order-1 my-4 lg:my-0">
+            <Image src={image9} className="" alt="" />
+          </div>
+          <div className="basis-2/3 space-y-5 flex flex-col justify-center items-center order-1 lg:order-2">
+            <h4 className="text-center font-kenao text-7xl text-[#2c8748]">
+              Goal IV
+            </h4>
+            <p className="text-center max-w-[90%]">
+              Craftroots provides a connection between people, not machines,
+              through every step of the way. From vegetables hand-picked and
+              transported by local farmers to the same vegetables being stocked
+              & tracked by our pantry chef, to the same vegetables being
+              prepared expertly by our chefs,
+            </p>
           </div>
         </div>
       </section>
+      <div className="w-full h-48 bg-repeat-x bg-[url('/about/10.png')] bg-[length:auto_100%]"></div>
     </main>
   );
 }
-
-//   return (
-//     <div>
-//       {
-//         //REPLACE ALL IMAGES
-//       }
-//       <div
-//         className={"relative h-[100vh]"}
-//         src={banner}
-//         speed={0}
-//       >
-//         <div
-//           className={
-//             "absolute inset-0 content-center justify-items-center bg-black/30"
-//           }
-//         >
-//           <h1
-//             className={"flex font-seasons text-6xl lg:text-8xl text-[#ffffff]"}
-//           >
-//             About Us
-//           </h1>
-//           <Image
-//             alt=""
-//             src={divider}
-//             className="mt-4 mb-1 w-3/4 max-w-[700px]"
-//           />
-//           <p className="text-center md:w-[70%] text-2xl text-white">
-//             Who we are, what we do, and why we do it.
-//           </p>
-//         </div>
-//       </div>
-//       <section className="flex">
-//         <div className=""></div>
-//         <div className="">
-//           <Image src={logo} alt="" className="" fill />
-//         </div>
-//       </section>
-//       <section></section>
-//       <section></section>
-
-//       {/* <div
-//         className={
-//           "flex-col h-[400vh] md:h-[200vh] w-full flex justify-center items-center"
-//         }
-//       >
-//         <div className={"flex"}></div>
-//         <div className={"m-5 bg-white w-[95%] md:w-[70vw] h-[50vh] "}>
-//           <div className={"m-5 h-full flex flex-col md:flex-row"}>
-//             <div className={"relative  h-full flex-row md:flex-col pr-2"}>
-//               <h2 className="p-4 font-dm text-4xl md:text-6xl text-[#03402a]">
-//                 Who We Are
-//               </h2>
-//               <p>
-//                 Craftroots&apos; team is primarily composed of those with Asian
-//                 heritage, including members from India, China, Iraq, the
-//                 Phillippines, and more. We are highly committed to sharing our
-//                 cultural heritage through cuisine, along with adding our own
-//                 vegetarian and sustainable twist to it.
-//               </p>
-//               <p>
-//                 We also partner with a multitude of local farms and businesses
-//                 to best serve the community as a whole in more ways than one.
-//               </p>
-
-//               <Link
-//                 href="/farm-to-table"
-//                 target=""
-//                 locale=""
-//                 className={
-//                   "-translate-y-10 text-white absolute left-0 bottom-0 h-[5vh] w-[20vw] content-center text-center bg-[#03402a]"
-//                 }
-//               >
-//                 Farm-to-Table
-//               </Link>
-//             </div>
-//             <Image src={logo} alt="" className=" h-full pb-10" />
-//             {
-//               // replace this image later.
-//             }
-//           </div>
-//         </div>
-//         <Image alt="" src={divider} className="mt-4 mb-1 w-3/4" />
-//         <div className={"m-5 bg-white w-[90vw] h-[100vh] md:h-[50vh]"}>
-//           <div className={"m-5 h-full flex flex-col md:flex-row"}>
-//             <Image src={kitchen} alt="" className=" pb-10" />
-//             {
-//               // replace this image later.
-//             }
-//             <div className={"relative w-full md:w-1/2 h-1/2 md:h-full pl-4"}>
-//               <h2 className="p-4 font-dm text-4xl md:text-6xl text-right text-[#03402a]">
-//                 What We Do
-//               </h2>
-//               <p>
-//                 Here at Craftroots, we take the utmost care and pride in our
-//                 ingredients. This isn&apos;t just limited to sourcing produce
-//                 locally -- the way we prepare those ingredients to create
-//                 something new is just as important.
-//               </p>
-//               <p>
-//                 We place great care in our preparation, ensuring that our dishes
-//                 are not only maximizing the freshness of seasonal produce, but
-//                 also combining our cultural heritage with the local culture, all
-//                 while adding our own unique twist to it.
-//               </p>
-
-//               <Link
-//                 href="/preparation"
-//                 target=""
-//                 locale=""
-//                 className={
-//                   "-translate-y-10 text-white absolute right-0 bottom-0 h-[5vh] w-[20vw] content-center text-center bg-[#03402a]"
-//                 }
-//               >
-//                 Preparation
-//               </Link>
-//             </div>
-//           </div>
-//         </div>
-//         <Image alt="" src={divider} className="mt-4 mb-1 w-3/4" />
-//         <div className={"m-5 bg-white w-[90vw] h-[50vh]"}>
-//           <div className={"m-5 h-full flex flex-col md:flex-row"}>
-//             <div className={"relative w-1/2 h-full pr-2"}>
-//               <h2 className="p-4 font-dm text-4xl md:text-6xl text-[#03402a]">
-//                 Our Mission
-//               </h2>
-//               <p>
-//                 Our mission at Craftroots is to benefit not only our customers
-//                 and business partners, but our local community as a whole. By
-//                 providing delicious seasonal dishes to our customers, purhcasing
-//                 in-season produce from local farms, and partnering with other
-//                 local businesses, we are able to in part provide for the
-//                 community while also providing for ourselves.
-//               </p>
-//               <p>
-//                 We also seek to promote sustainable practices in every step of
-//                 the way, including solar power, composting, recycled goods, and
-//                 more. We encourage use, reuse, and recycling in all of our
-//                 practices.
-//               </p>
-
-//               <Link
-//                 href="/sustainability"
-//                 target=""
-//                 locale=""
-//                 className={
-//                   "-translate-y-10 text-white absolute left-0 bottom-0 h-[5vh] w-[20vw] content-center text-center bg-[#03402a]"
-//                 }
-//               >
-//                 Sustainability
-//               </Link>
-//             </div>
-//             <Image
-//               src={earth}
-//               objectFit={"fill"}
-//               alt=""
-//               className="w-1/2 h-full pb-10"
-//             />
-//             {
-//               // replace this image later.
-//             }
-//           </div>
-//         </div>
-//       </div> */}
-//     </div>
-//   );
